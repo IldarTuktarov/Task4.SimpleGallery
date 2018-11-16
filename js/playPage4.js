861d5077129ceb47714fab61fc57b09b9ebd7954
Ildar Tuktarov <45071411+IldarTuktarov@users.noreply.github.com>
2018-11-17 00:11:05 +0300

    Delete playPage4.js

--- js/playPage4.js
+++
@@ -1,6 +0,0 @@
-previousButton.addEventListener("click", function () { redirect(pagesUrl[2]); });
-
-startOverBtn.addEventListener("click", function () { redirect(pagesUrl[0]); });
-
-closeBtn.addEventListener("click", function () { window.close(); });
-
